ca51d4269a8c80ebfc8b9084c3d0f8b1f53513eb
scott <scottswindell@email.arizona.edu>
2018-10-16 14:14:57 -0700

    script to send TLE's from file to TCS

---
+++ scripts/tle2tcs.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+from telescope import kuiper
+import sys
+import time
+
+usage = """Send TLE data from a file to TCSng.
+        usage: python tle2tcs.py <filename>
+        like:  python tle2tcs.py  iss.tle
+"""
+
+if len( sys.argv ) != 2:
+    print(usage)
+    sys.exit(1)
+
+
+with open(sys.argv[1]) as tlefd:
+    tle = tlefd.read()
+
+outtle = []
+test_list = tle.split('\n')
+if len(test_list) == 2:
+    outtle.append(test_list[0])
+    outtle.append(test_list[1])
+else:
+    outtle.append(test_list[1])
+    outtle.append(test_list[2])
+
+
+
+tel=kuiper()
+tel.comTLE(test_list)
+for a in range(10):
+    print("Satellite Horizontal Coordinates: {} {}" .format(*tle.reqGETSATELAZ()) )
+    time.sleep(1.0)
+
+
+
